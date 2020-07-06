7c9a8f5efca97c858cefb2d7a4b910f356f7d9a6
Daniel Silva <Daniel.SA.Silva@ctw.bmwgroup.com>
2020-07-06 23:48:09 +0100

    added responsiveness mobile to home and about me pages

--- src/app/components/aboutMe/aboutMe.js
+++ src/app/components/aboutMe/aboutMe.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
 import './aboutMe.scss'
+import { withNamespaces } from 'react-i18next';
+import Paper from '@material-ui/core/Paper'
+import image from '../../images/looking-at-computer.png'
+import Image from 'material-ui-image'
+import { height } from '@material-ui/system';
 
-export default class AboutMe extends Component {
-    render() {
-        return (
-            <section id="aboutMe">
-                <h1>About Me</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nisi distinctio beatae! Tempora, quos neque?</p>
-            </section>
-        )
-    }
+function AboutMe({ t }) {
+
+    return (
+        <section className="aboutMe" id="aboutMe">
+            <Paper className="main-paper" elevation={24}>
+                <div>
+                    <h1 className="h1">{t('aboutMe.title')}</h1>
+                </div>
+                <div className="main-paper__content">
+                    <div className="main-paper__image-container">
+                        <img className="main-paper__image" src={image}></img>
+                    </div>
+                    <div className="border"></div>
+                    <div className="main-paper__description">
+                        <p className="p"> {t('aboutMe.description')}</p>
+                    </div>
+                </div>
+
+            </Paper>
+        </section>
+    );
 }
+
+export default withNamespaces()(AboutMe);
